7fcad4dc4cb852bba0b620013bcd6bbaa2e11465
Nick Coughlin <coughlin.nicholas@gmail.com>
2022-05-10 10:32:05 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// components
+import PeopleDataTable from "./components/PeopleDataTable";
+
+// styles
+import "./styles/loading.css";
+import "./styles/data-table.css";
+
+function App() {
+  return <PeopleDataTable />;
+}
+
+export default App;
